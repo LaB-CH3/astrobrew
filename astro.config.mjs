a141b035752c65cb6ca6609f1805eb7d070eb9f0
dest92 <acebalmatias2019@gmail.com>
2024-11-03 21:18:34 -0300

    fix some things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify/functions";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  integrations: [tailwind()]
 });
