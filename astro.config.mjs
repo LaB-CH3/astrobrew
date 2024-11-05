1ae39c8882667ecef31583486e695a118b1c80f0
dest92 <acebalmatias2019@gmail.com>
2024-11-05 09:10:51 -0300

    add gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind({ applyBaseStyles: false }), react(), icon()],
 });
