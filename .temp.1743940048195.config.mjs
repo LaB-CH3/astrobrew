2650efb22d5ad2af193b581327d5337d42a143f6
Anthony Charles <anthonylannn@gmail.com>
2025-04-06 13:53:06 +0200

    upgraded to astro 5.6

--- .temp.1743940048195.config.mjs
+++ .temp.1743940048195.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
